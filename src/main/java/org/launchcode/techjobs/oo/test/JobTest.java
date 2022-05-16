98116392e44647c60586254a920bafbc59bf4150
LucasPBernard <lucas.p.bernard@gmail.com>
2022-05-16 03:29:36 -0500

    commit 03

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,14 +1,32 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.Job;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+    @Test
+    public void testSettingJobId() {
+        Job test_job_01 = new Job();
+        Job test_job_02 = new Job();
 
-    
+        assertEquals(test_job_01.getId(),1);
+        assertEquals(test_job_02.getId(),2);
+
+        assertTrue(test_job_02.getId()== test_job_01.getId()+1);
+
+
+        assertFalse(test_job_01.getId()==(test_job_02.getId()) );
+    }
+
+
 
 }
+
