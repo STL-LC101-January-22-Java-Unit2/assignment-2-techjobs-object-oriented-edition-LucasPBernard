04fe1b75e7da95836a9105ba77902ec1624ac227
LucasPBernard <lucas.p.bernard@gmail.com>
2022-05-16 05:34:07 -0500

    commit 04

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -20,12 +20,16 @@ public class PositionType {
 
     // TODO: Add a custom toString() method that returns the data stored in 'value'.
 
-    @Override
-    public String toString() {
-        return "PositionType{" +
-                "value='" + value + '\'' +
-                '}';
-    }
+//    @Override
+//    public String toString() {
+//        return "PositionType{" +
+//                "value='" + value + '\'' +
+//                '}';
+//    }
+@Override
+public String toString() {
+    return value;
+}
 
 
     // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -3,7 +3,7 @@ package org.launchcode.techjobs.oo.test;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.launchcode.techjobs.oo.Job;
+import org.launchcode.techjobs.oo.*;
 
 import static org.junit.Assert.*;
 
@@ -17,16 +17,26 @@ public class JobTest {
         Job test_job_01 = new Job();
         Job test_job_02 = new Job();
 
-        assertEquals(test_job_01.getId(),1);
-        assertEquals(test_job_02.getId(),2);
+//        assertEquals(test_job_01.getId(),1);
+//        assertEquals(test_job_02.getId(),2);
 
         assertTrue(test_job_02.getId()== test_job_01.getId()+1);
 
 
         assertFalse(test_job_01.getId()==(test_job_02.getId()) );
     }
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job aJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
 
+        assertEquals(aJob.getName(),"Product tester");
+        assertEquals(aJob.getEmployer().getValue(),"ACME");
+        assertEquals(aJob.getLocation().getValue(),"Desert");
+        assertEquals(aJob.getPositionType().getValue(),"Quality control");
+        assertEquals(aJob.getCoreCompetency().getValue(),"Persistence");
+        assert aJob instanceof Job;
 
-
+    }
+    
 }
 
