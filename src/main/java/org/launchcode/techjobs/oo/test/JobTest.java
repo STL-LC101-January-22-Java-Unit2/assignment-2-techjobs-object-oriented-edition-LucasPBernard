fcbec977f536d70302af6fc1169ebbf2804f6109
LucasPBernard <lucas.p.bernard@gmail.com>
2022-06-05 04:29:58 -0500

    final check

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -90,15 +90,21 @@ public class JobTest {
 
     @Test
     public void testToStringHandlesEmptyField() {
-        Job job = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job job = new Job();
         Job job2 = new Job(null, new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
-        assertEquals(job.toString(),"Data not available");
+        Job job3 = new Job("Product tester",null, new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertEquals(job2.toString(),"\nID: 2\n" +
+                "Name: Data not available\n" +
+                "Employer: ACME\n" +
+                "Location: Desert\n" +
+                "Position Type: Quality control\n" +
+                "Core Competency: Persistence\n");
         assertEquals(job2.getPositionType().toString(),"Quality control");
+        assertEquals(job2.getEmployer().toString(),"ACME");
+        assertNull(job2.getName());
         assertTrue(job2.toString().contains("Data not available"));
-
-//        System.out.println(job.toString());
-//        System.out.println(job2.toString());
-
+        //assertTrue(job3.toString().contains("Data not available"));
+        //assertTrue(job.toString().equals("OOPS! This job does not seem to exist."));
     }
 
 
